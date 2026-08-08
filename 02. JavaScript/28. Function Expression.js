d7d336e6c421b20310b97ab3ce8364aab1fca6f2
Umma Salma Mim <144950802+ummasalmamim@users.noreply.github.com>
2026-08-09 05:17:22 +0600

    Add files via upload

---
+++ 02. JavaScript/28. Function Expression.js
@@ -0,0 +1,38 @@
+
+//ekta variable er bhitore jodi ekta function ke declare kora hoy take function expression bole
+
+
+var expression_mim= function Callme(){
+
+}
+
+function Callme(){
+    console.log("My name is Mim")
+}
+
+Callme()
+
+
+console.log("===============")
+
+//function exoression
+
+var expression_mim= function (){
+
+    console.log("My name is Jim")
+}
+
+expression_mim()
+
+console.log("======================")
+
+// parameter appl in function expression
+
+
+
+var expression_mim= function (name1,name2){
+
+    console.log(name1, name2)
+}
+
+expression_mim("Mohammadpur","Badda")
